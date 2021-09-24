70950ba1ea88d4984653759ddd782215e339f4c2
Ceren Jana Akkuec <cjakkuec@gmail.com>
2021-09-24 10:43:38 +0200

    add javascript pain

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ links.forEach((link) => {
   });
 });
 
-/* Bookmark Toggle */
+/* Bookmark Toggle
 
 const bookmarks = document.querySelectorAll(".bookmark");
 
@@ -36,9 +36,9 @@ bookmarks.forEach((bookmark) => {
   bookmark.addEventListener("click", () => {
     bookmark.classList.toggle("checked");
   });
-});
+}); */
 
-/* Answer Toggle */
+/* Answer Toggle 
 
 const answerSections = document.querySelectorAll(".answer-section");
 
@@ -48,7 +48,108 @@ answerSections.forEach((answerSection) => {
   button.addEventListener("click", () => {
     answer.classList.toggle("show");
   });
+}); */
+
+/* Form Script */
+
+const form = document.querySelector("form");
+
+const questionCards = [
+  { newQuestion: "Question 1", newAnswer: "Answer 1", newTags: "1,2,3,4" },
+
+  {
+    newQuestion: "Question 2",
+    newAnswer: "Answer 2",
+    newTags: "1,2,3,4",
+  },
+];
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const newInput = {
+    newQuestion: form.elements.newquestion.value,
+    newAnswer: form.elements.newanswer.value,
+    newTags: form.elements.newtags.value,
+  };
+
+  questionCards.push(newInput);
+
+  renderQuestions();
+
+  document.querySelector("form").reset();
 });
+
+/* Question Template */
+
+const createQuestionsHtml = (questionCards) => {
+  let html = "";
+
+  questionCards.forEach((questionCard) => {
+    /*Tag Array + Split */
+    const tags = questionCard.newTags.split(",");
+    let tagsList = "";
+    tags.forEach((tag) => {
+      tagsList = tagsList + `<li><a href="">${tag}</a></li>`;
+    });
+    console.log(tags);
+
+    html =
+      html +
+      `
+    <article class="questions">
+          <img class="bookmark" />
+          <h3>Question</h3>
+          <div>
+            <p>
+            ${questionCard.newQuestion}
+            </p>
+            <div class="answer-section">
+              <button class="answer-toggle">Show Answer</button>
+              <p class="answer">
+              ${questionCard.newAnswer}
+              </p>
+            </div>
+            <div class="tags">
+              <ul>
+                ${tagsList}
+              </ul>
+            </div>
+          </div>
+        </article>
+    `;
+  });
+
+  return html;
+};
+
+const renderQuestions = () => {
+  const questionsHtml = createQuestionsHtml(questionCards);
+  const questionsContainer = document.querySelector("#questions-home");
+  questionsContainer.innerHTML = questionsHtml;
+
+  const answerSections = document.querySelectorAll(".answer-section");
+
+  /* Answer Toggle*/
+  answerSections.forEach((answerSection) => {
+    const button = answerSection.querySelector(".answer-toggle");
+    const answer = answerSection.querySelector(".answer");
+    button.addEventListener("click", () => {
+      answer.classList.toggle("show");
+    });
+  });
+
+  /*Bookmark Toggle */
+  const bookmarks = document.querySelectorAll(".bookmark");
+
+  bookmarks.forEach((bookmark) => {
+    bookmark.addEventListener("click", () => {
+      bookmark.classList.toggle("checked");
+    });
+  });
+};
+
+renderQuestions();
 
 /* Active State (Fade-In) (Alternative 2 + Fade-In ausprobieren)*/
 
